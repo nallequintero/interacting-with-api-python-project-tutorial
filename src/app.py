84ae80970e642cd458818b36c3031adb0029347e
Nallely <61744223+nallequintero@users.noreply.github.com>
2025-02-03 15:12:29 +0000

    tarea finalizada

--- src/app.py
+++ src/app.py
@@ -2,6 +2,37 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import seaborn as sns
 
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+
+spoti = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id,
+                                                              client_secret = client_secret))
+
+artist_id = "4q3ewBCX7sLwd24euuV69X"
+
+response = spoti.artist_top_tracks("4q3ewBCX7sLwd24euuV69X")
+if response:
+  # We keep the "tracks" object of the answer
+  tracks = response["tracks"]
+  # We select, for each song, the data we are interested in and discard the rest
+  tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+
+
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+
+print(tracks_df.head(10))
+
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
+#No parece tener relacion la duracion de la cancion con la popularidad
